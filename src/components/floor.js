b6afcc6c046c8e02117bdc2c37accbcb54c385a6
Ruben190404 <92361480+Ruben190404@users.noreply.github.com>
2023-05-24 14:07:30 +0200

    byuhnij

--- src/components/floor.js
+++ src/components/floor.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Floor(props) {
     return (
-        <mesh {...props} receiveShadow={true}>
+        <mesh {...props} receiveShadow={true} onClick={(e)=>e.stopPropagation()}>
             <boxGeometry args={[20, 1, 20]}/>
             <meshPhysicalMaterial color="green"/>
         </mesh>
---
+++ src/components/tempChart.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Chart from "chart.js/auto";
+import {Line} from "react-chartjs-2";
+
+function TempChart() {
+    return (
+        <Line
+            data={
+                {
+                    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+                    datasets: [
+                        {
+                            label: 'Temperature average',
+                            fill: false,
+                            lineTension: 0.1,
+                            backgroundColor: 'rgba(255,0,0,0.4)',
+                            borderColor: 'rgba(255,0,0,1)',
+                            borderCapStyle: 'butt',
+                            borderDash: [],
+                            borderDashOffset: 0.0,
+                            borderJoinStyle: 'miter',
+                            pointBorderColor: 'rgba(255,0,0,1)',
+                            pointBackgroundColor: '#fff',
+                            pointBorderWidth: 1,
+                            pointHoverRadius: 5,
+                            pointHoverBackgroundColor: 'rgba(255,0,0,1)',
+                            pointHoverBorderColor: 'rgba(220,220,220,1)',
+                            pointHoverBorderWidth: 2,
+                            pointRadius: 3,
+                            pointHitRadius: 10,
+                            data: [21, 22, 19, 16, 5, 25, 0]
+                        }
+                    ]
+                }
+            }
+        />
+    )
+}
+
+export default TempChart;
